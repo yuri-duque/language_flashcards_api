9a765627b559b040b944f511b368232ffa91b796
YURI DUQUE <yuri.duque@portoseguro.com.br>
2024-10-23 20:51:00 -0300

    atualizando routes

--- src/routes/languageRouter/index.ts
+++ src/routes/languageRouter/index.ts
@@ -1,54 +1,46 @@
+import authMiddleware from '@middlewares/authMiddleware';
 import { LanguageService } from '@services/languageService';
 import { validationQueryParams } from '@utils/validationRequest';
-import { Response, Router } from 'express';
-import { RequestWithQuery } from '../../../@types/express';
+import { Request, Response, Router } from 'express';
 import { ExemplesQueryParams, TranslateQueryParams } from './types';
 import { requestExempleSchema, requestTranslateSchema } from './validationSchemas';
 
 const languageRouter = Router();
 
-languageRouter.get(
-  '/language/exemples',
-  async (req: RequestWithQuery<ExemplesQueryParams>, res: Response) => {
-    try {
-      const params = req.query;
-      await validationQueryParams(params, requestExempleSchema);
+languageRouter.get('/language/exemples', authMiddleware, async (req: Request, res: Response) => {
+  try {
+    const params = await validationQueryParams<ExemplesQueryParams>(req, requestExempleSchema);
 
-      const languageService = new LanguageService();
-      const exemples = await languageService.generateExemples(params);
+    const languageService = new LanguageService();
+    const exemples = await languageService.generateExemples(params);
 
-      const response = {
-        text: params.text,
-        exemples,
-      };
+    const response = {
+      text: params.text,
+      exemples,
+    };
 
-      res.success({ data: response });
-    } catch (error) {
-      res.error({ error: error as Error, message: 'Error to generate exemples' });
-    }
-  },
-);
+    res.success({ data: response });
+  } catch (error) {
+    res.error({ error: error as Error, message: 'Error to generate exemples' });
+  }
+});
 
-languageRouter.get(
-  '/language/translate',
-  async (req: RequestWithQuery<TranslateQueryParams>, res: Response) => {
-    try {
-      const params = req.query;
-      await validationQueryParams(params, requestTranslateSchema);
+languageRouter.get('/language/translate', authMiddleware, async (req: Request, res: Response) => {
+  try {
+    const params = await validationQueryParams<TranslateQueryParams>(req, requestTranslateSchema);
 
-      const languageService = new LanguageService();
-      const translatedText = await languageService.translate(params);
+    const languageService = new LanguageService();
+    const translatedText = await languageService.translate(params);
 
-      const response = {
-        text: params.text,
-        translatedText,
-      };
+    const response = {
+      text: params.text,
+      translatedText,
+    };
 
-      res.success({ data: response });
-    } catch (error) {
-      res.error({ error: error as Error, message: 'Error to translate' });
-    }
-  },
-);
+    res.success({ data: response });
+  } catch (error) {
+    res.error({ error: error as Error, message: 'Error to translate' });
+  }
+});
 
 export { languageRouter };
--- src/utils/validationRequest/index.ts
+++ src/utils/validationRequest/index.ts
@@ -1,9 +1,15 @@
 import CustomError from '@utils/customError';
+import { Request } from 'express';
 import { AnyObjectSchema } from 'yup';
 
-export const validationQueryParams = async (params: any, validationSchema: AnyObjectSchema) => {
+export const validationQueryParams = async <T>(
+  request: Request,
+  validationSchema: AnyObjectSchema,
+) => {
   try {
+    const params = request.query as unknown as T;
     await validationSchema.validate(params);
+    return params;
   } catch (error: any) {
     throw new CustomError(error.message);
   }
